7698c3b5787f38e47f4657c4b0c086518973e08d
Prasad Mistry <pmistry21@myseneca.ca>
2024-11-25 00:33:14 -0500

    Added lab7i

---
+++ lab7i.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Name: Prasad Mistry
+# Student Number: 111964193
+# Student Email: pmistry21@myseneca.ca
+# Course: OPS445
+# Section: ZAA
+# Semester: Fall 2024
+
+def function1():
+    global schoolName
+    schoolName = 'SICT'
+    print('print() in function1 on schoolName:',schoolName)
+
+def function2():
+    global schoolName
+    schoolName = 'SSDO'
+    print('print() in function2 on schoolName:',schoolName)
+
+schoolName = 'Seneca'
+print('print() in main on schoolName:',schoolName)
+function1()
+print('print() in main on schoolName:',schoolName)
+function2()
+print('print() in main on schoolName:',schoolName)
