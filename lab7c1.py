18f066dc416b8bb2720c9fafa8951bed798400c7
Prasad Mistry <pmistry21@myseneca.ca>
2024-11-24 23:58:06 -0500

    Completed lab7c1.py

---
+++ lab7c1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Name: Prasad Mistry
+# Student Number: 111964193
+# Student Email: pmistry21@myseneca.ca
+# Course: OPS445
+# Section: ZAA
+# Semester: Fall 2024
+
+from lab7c import *
+
+t1 = Time(8,0,0)
+t2 = Time(8,55,0)
+t3 = Time(9,50,0)
+
+td = Time(0,50,0)
+
+tsum1 = sum_times(t1,td)
+tsum2 = sum_times(t2,td)
+t3None = change_time(t3,1800)   
+
+ft = format_time
+print(ft(t1),'+',ft(td),'-->',ft(tsum1))
+print(ft(t2),'+',ft(td),'-->',ft(tsum2))
+print('09:50:00 + 1800 sec','-->',ft(t3))
